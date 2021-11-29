9c917ed5f5407ac44da30626a114c6d42cbd5ec0
Alex Rozgo <alex.rozgo@gmail.com>
2021-11-29 00:53:25 -0500

    visibility for axes

--- crates/simula_viz/axes.rs
+++ crates/simula_viz/axes.rs
@@ -17,6 +17,7 @@ impl Default for Axes {
 #[derive(Bundle)]
 pub struct AxesBundle {
     pub axes: Axes,
+    pub visible: Visible,
     pub transform: Transform,
     pub global_transform: GlobalTransform,
 }
@@ -25,22 +26,25 @@ impl Default for AxesBundle {
     fn default() -> Self {
         AxesBundle {
             axes: Default::default(),
+            visible: Visible { is_visible: true, is_transparent: false },
             transform: Default::default(),
             global_transform: Default::default(),
         }
     }
 }
 
-pub fn system(mut lines: ResMut<line::Lines>, query: Query<(&Axes, &Transform)>) {
-    for (axes, transform) in query.iter() {
-        let f_start = transform.translation + transform.forward() * axes.inner_offset;
-        let r_start = transform.translation + transform.right() * axes.inner_offset;
-        let u_start = transform.translation + transform.up() * axes.inner_offset;
-        let f_end = f_start + transform.forward() * axes.size;
-        let r_end = r_start + transform.right() * axes.size;
-        let u_end = u_start + transform.up() * axes.size;
-        lines.line_colored(f_start, f_end, 0., Color::BLUE);
-        lines.line_colored(r_start, r_end, 0., Color::RED);
-        lines.line_colored(u_start, u_end, 0., Color::GREEN);
+pub fn system(mut lines: ResMut<line::Lines>, query: Query<(&Axes, &GlobalTransform, &Visible)>) {
+    for (axes, transform, visible) in query.iter() {
+        if visible.is_visible {
+            let f_start = transform.translation + transform.forward() * axes.inner_offset;
+            let r_start = transform.translation + transform.right() * axes.inner_offset;
+            let u_start = transform.translation + transform.up() * axes.inner_offset;
+            let f_end = f_start + transform.forward() * axes.size;
+            let r_end = r_start + transform.right() * axes.size;
+            let u_end = u_start + transform.up() * axes.size;
+            lines.line_colored(f_start, f_end, 0., Color::BLUE);
+            lines.line_colored(r_start, r_end, 0., Color::RED);
+            lines.line_colored(u_start, u_end, 0., Color::GREEN);
+        }
     }
 }
