d056d866e2291412e1121d7dfbbab490c4adfa78
Alex Rozgo <alex.rozgo@gmail.com>
2022-09-21 21:35:03 -0600

    WIP mission AI

---
+++ tools/mission/src/main.rs
@@ -0,0 +1,136 @@
+use bevy::{
+    diagnostic::{Diagnostics, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+};
+use bevy_inspector_egui::WorldInspectorPlugin;
+use enum_iterator::IntoEnumIterator;
+use simula_action::ActionPlugin;
+use simula_camera::orbitcam::*;
+use simula_net::NetPlugin;
+use simula_viz::{
+    axes::{Axes, AxesBundle, AxesPlugin},
+    grid::{Grid, GridBundle, GridPlugin},
+    lines::{LineMesh, LinesMaterial, LinesPlugin},
+};
+
+fn main() {
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "[Simbotic] Simula - Sandbox".to_string(),
+            width: 940.,
+            height: 528.,
+            ..Default::default()
+        })
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(ClearColor(Color::rgb(0.105, 0.10, 0.11)))
+        .add_plugins(DefaultPlugins)
+        .add_plugin(NetPlugin)
+        .add_plugin(WorldInspectorPlugin::new())
+        .add_plugin(ActionPlugin)
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_plugin(OrbitCameraPlugin)
+        .add_plugin(LinesPlugin)
+        .add_plugin(AxesPlugin)
+        .add_plugin(GridPlugin)
+        .add_startup_system(setup)
+        .add_system(debug_info)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut lines_materials: ResMut<Assets<LinesMaterial>>,
+    line_mesh: Res<LineMesh>,
+    asset_server: Res<AssetServer>,
+) {
+    // grid
+    commands
+        .spawn_bundle(GridBundle {
+            grid: Grid {
+                size: 10,
+                divisions: 10,
+                start_color: Color::DARK_GRAY,
+                end_color: Color::DARK_GRAY,
+                ..Default::default()
+            },
+            mesh: meshes.add(line_mesh.clone()),
+            material: lines_materials.add(LinesMaterial {}),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..Default::default()
+        })
+        .insert(Name::new("Grid"));
+
+    // axes
+    commands
+        .spawn_bundle(AxesBundle {
+            axes: Axes {
+                size: 1.,
+                inner_offset: 5.,
+            },
+            mesh: meshes.add(line_mesh.clone()),
+            material: lines_materials.add(LinesMaterial {}),
+            transform: Transform::from_xyz(0.0, 0.01, 0.0),
+            ..Default::default()
+        })
+        .insert(Name::new("Axes: World"));
+
+    let theta = std::f32::consts::FRAC_PI_4;
+    let light_transform = Mat4::from_euler(EulerRot::ZYX, 0.0, std::f32::consts::FRAC_PI_2, -theta);
+    commands.spawn_bundle(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            color: Color::rgb(1.0, 1.0, 1.0),
+            illuminance: 5000.,
+            ..Default::default()
+        },
+        transform: Transform::from_matrix(light_transform),
+        ..Default::default()
+    });
+
+    // orbit camera
+    commands
+        .spawn_bundle(Camera3dBundle {
+            ..Default::default()
+        })
+        .insert(OrbitCamera {
+            center: Vec3::new(0.0, 1.0, 0.0),
+            distance: 10.0,
+            ..Default::default()
+        });
+
+    // FPS on screen
+    commands.spawn_bundle(TextBundle {
+        text: Text {
+            sections: vec![TextSection {
+                value: "\nFPS: ".to_string(),
+                style: TextStyle {
+                    font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                    font_size: 12.0,
+                    color: Color::rgb(0.0, 1.0, 0.0),
+                },
+            }],
+            ..Default::default()
+        },
+        style: Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                top: Val::Px(5.0),
+                left: Val::Px(5.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+}
+
+fn debug_info(diagnostics: Res<Diagnostics>, mut query: Query<&mut Text>) {
+    if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+        if let Some(average) = fps.average() {
+            for mut text in query.iter_mut() {
+                text.sections[0].value = format!("{:.2}", average);
+            }
+        }
+    };
+}
