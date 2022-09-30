c9aedd846b1a6da675953fd792af8726055b89fd
Alex Rozgo <alex.rozgo@gmail.com>
2022-09-30 17:03:56 -0600

    technical analysis for missions

--- crates/simula_mission/src/lib.rs
+++ crates/simula_mission/src/lib.rs
@@ -1,9 +1,16 @@
-
-
 #[cfg(test)]
 mod tests {
+    use ta::indicators::ExponentialMovingAverage;
+    use ta::Next;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        // it can return an error, when an invalid length is passed (e.g. 0)
+        let mut ema = ExponentialMovingAverage::new(3).unwrap();
+
+        assert_eq!(ema.next(2.0), 2.0);
+        assert_eq!(ema.next(5.0), 3.5);
+        assert_eq!(ema.next(1.0), 2.25);
+        assert_eq!(ema.next(6.25), 4.25);
     }
 }
