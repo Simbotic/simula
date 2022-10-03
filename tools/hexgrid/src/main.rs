c4e6e9727bfd2ee62ed5d67a3e98331a7e04141a
Van <donovandoroin@yahoo.com>
2022-10-04 04:52:10 +0800

    Inside the hexgrid setup, modified orbitcamera fields

--- tools/hexgrid/src/main.rs
+++ tools/hexgrid/src/main.rs
@@ -69,7 +69,9 @@ pub fn hexgrid_setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
             ..default()
         })
         .insert(OrbitCamera {
+            y: 0.01,
             pan_sensitivity: 10.0,
+            distance: 50.0,
             center: Vec3::ZERO,
             ..Default::default()
         })
