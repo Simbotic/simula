ba75c945bd9a2955e1edbd9d0672b7749b9248a1
Alex Rozgo <alex.rozgo@gmail.com>
2023-05-08 23:54:19 -0600

    db inspectable

--- crates/simula_surrealdb/src/lib.rs
+++ crates/simula_surrealdb/src/lib.rs
@@ -12,63 +12,99 @@ pub struct SurrealPlugin;
 
 impl Plugin for SurrealPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup).add_system(run);
+        app.register_type::<SurrealClient>()
+            .add_startup_system(client_connector)
+            .add_system(client_receiver);
     }
 }
 
-#[derive(Resource, Deref)]
-struct SurrealClient(Surreal<Client>);
+#[derive(Reflect, Resource, Debug, Default, Clone)]
+#[reflect(Resource, Debug)]
+pub struct SurrealClient {
+    #[reflect(ignore)]
+    client: Option<Surreal<Client>>,
+    pub endpoint: String,
+    pub version: String,
+    pub namespace: String,
+    pub database: String,
+    pub username: String,
+    pub password: String,
+}
 
 #[derive(Resource, Deref)]
-struct SurrealClientReceiver(Receiver<Surreal<Client>>);
+struct SurrealClientReceiver(Receiver<SurrealClient>);
 
-fn setup(mut commands: Commands) {
+fn client_connector(mut commands: Commands) {
     let (sender, receiver) = bounded(1);
     commands.insert_resource(SurrealClientReceiver(receiver));
 
     let thread_pool = AsyncComputeTaskPool::get();
     let _ = thread_pool.spawn(async move {
-        if let Ok(client) = Surreal::new::<Ws>("127.0.0.1:8000").await {
+        let address = "127.0.0.1:8000";
+
+        if let Ok(client) = Surreal::new::<Ws>(address).await {
             info!("Connected to SurrealDB");
+
+            let version = if let Ok(version) = client.version().await {
+                version.to_string()
+            } else {
+                "unknown".into()
+            };
+
+            let client_resource = SurrealClient {
+                client: Some(client.clone()),
+                endpoint: address.to_string(),
+                version,
+                namespace: "default".into(),
+                database: "default".into(),
+                username: "root".into(),
+                password: "root".into(),
+                ..default()
+            };
 
             let _ = client
                 .signin(Root {
-                    username: "root",
-                    password: "root",
+                    username: &client_resource.username,
+                    password: &client_resource.password,
                 })
                 .await;
-            let _ = client.use_ns("default").use_db("default").await;
+            let _ = client
+                .use_ns(&client_resource.namespace)
+                .use_db(&client_resource.database)
+                .await;
 
-            sender.send(client).unwrap();
+            sender.send(client_resource).unwrap();
         };
     });
 }
 
-fn run(mut commands: Commands, surreal_client_receiver: Res<SurrealClientReceiver>) {
+fn client_receiver(mut commands: Commands, surreal_client_receiver: Res<SurrealClientReceiver>) {
     if let Some(client) = surreal_client_receiver.try_recv().ok() {
         info!("Surreal client received");
 
-        commands.insert_resource(SurrealClient(client.clone()));
+        commands.insert_resource(client.clone());
 
-        let thread_pool = AsyncComputeTaskPool::get();
-        let _ = thread_pool.spawn(async move {
-            let created: Result<Vec<Record>, surrealdb::Error> = client
-                .create("person")
-                .content(Person {
-                    title: "Researcher",
-                    name: Name {
-                        first: "Alex",
-                        last: "Rozgo",
-                    },
-                    simulation: true,
-                })
-                .await;
-            if let Ok(created) = created {
-                info!("Created: {:?}", created);
-            } else if let Err(e) = created {
-                error!("{:?}", e);
-            }
-        });
+        if let Some(client) = client.client {
+            let thread_pool = AsyncComputeTaskPool::get();
+            let _ = thread_pool.spawn(async move {
+                let created: Result<Vec<Record>, surrealdb::Error> = client
+                    .create("person")
+                    .content(Person {
+                        title: "Researcher",
+                        name: Name {
+                            first: "Alex",
+                            last: "Rozgo",
+                        },
+                        simulation: true,
+                    })
+                    .await;
+                if let Ok(created) = created {
+                    info!("Created: {:?}", created);
+                } else if let Err(e) = created {
+                    error!("{:?}", e);
+                }
+            });
+        }
     }
 }
 
